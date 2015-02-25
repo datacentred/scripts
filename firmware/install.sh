a3f13b29a01bc83b7c206681295b94dfa9958133
Max Siegieda <max.siegieda@datacentred.co.uk>
2015-02-25 15:25:39 +0000

    fix the hardcoded dirs, just remember to run from the same dir as the script

--- firmware/install.sh
+++ firmware/install.sh
@@ -7,7 +7,7 @@ mkdir -p ${FWHOME}/bios/${BIOSFWVER}
 mkdir -p ${FWHOME}/nvram
 mkdir -p /usr/local/bin/scripts/firmware
 
-install -m 644 firmware/X8DTT220.ima ${FWHOME}/ipmi/${IPMIFWVER}
-install -m 755 firmware/Yafuflash /usr/local/bin/scripts/firmware
-install -m 644 bios_21b.bin ${FWHOME}/ipmi/${IPMIFWVER}/dcbios.bin
+install -m 644 yafuflash/X8DTT220.ima ${FWHOME}/ipmi/${IPMIFWVER}
+install -m 755 yafuflash/Yafuflash /usr/local/bin/scripts/firmware
+install -m 644 bios_21b.bin ${FWHOME}/bios/${BIOSFWVER}/dcbios.bin
 install -m 644 dcnvram_080714.bin ${FWHOME}/nvram/dcnvram.bin
